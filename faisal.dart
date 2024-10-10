25657d1ba50b6470077963ab010a5a6b2e36c34e
Faisal <faisalmuhamad400@gmail.com>
2024-10-10 10:05:20 +0700

    Mengubah class pada file dart

--- faisal.dart
+++ faisal.dart
@@ -3,6 +3,12 @@ class Mahasiswa {
   var npm;
   var jurusan;
 
+  Mahasiswa(var nama, var npm, var jurusan) {
+    this.nama = nama;
+    this.npm = npm;
+    this.jurusan = jurusan;
+  }
+
   void infoMahasiswa() {
     print("Nama : $nama");
     print("NPM : $npm");
@@ -11,10 +17,9 @@ class Mahasiswa {
 }
 
 void main() {
-  Mahasiswa mhs1 = new Mahasiswa();
-  mhs1.nama = "Faisal";
-  mhs1.npm = "5520122110";
-  mhs1.jurusan = "Informatika";
+  Mahasiswa mhs1 = new Mahasiswa("Faisal", "5520122110", "Informatika");
+
+  mhs1.nama = "KM";
 
   mhs1.infoMahasiswa();
 }
