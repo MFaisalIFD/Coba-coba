3b87069a64719550b1790af9309fd681adf357be
Faisal <faisalmuhamad400@gmail.com>
2024-10-10 10:01:26 +0700

    Menambah class pada file dart

--- faisal.dart
+++ faisal.dart
@@ -1,3 +1,20 @@
-void main(){
-  
+class Mahasiswa {
+  var nama;
+  var npm;
+  var jurusan;
+
+  void infoMahasiswa() {
+    print("Nama : $nama");
+    print("NPM : $npm");
+    print("Jurusan : $jurusan");
+  }
+}
+
+void main() {
+  Mahasiswa mhs1 = new Mahasiswa();
+  mhs1.nama = "Faisal";
+  mhs1.npm = "5520122110";
+  mhs1.jurusan = "Informatika";
+
+  mhs1.infoMahasiswa();
 }
